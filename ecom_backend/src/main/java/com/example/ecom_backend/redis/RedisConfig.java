c188b5027bfe4e293348b423c9fc457ebeb81ac6
Surya Tejess Karri <surya.tejesskarri@vedantu.com>
2026-06-08 12:17:40 +0530

    bug: fix pom.xml

--- ecom_backend/src/main/java/com/example/ecom_backend/redis/RedisConfig.java
+++ ecom_backend/src/main/java/com/example/ecom_backend/redis/RedisConfig.java
@@ -1,11 +1,18 @@
 package com.example.ecom_backend.redis;
 
+import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.codec.ByteArrayCodec;
+import io.lettuce.core.codec.RedisCodec;
+import io.lettuce.core.codec.StringCodec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
@@ -35,6 +42,15 @@ public class RedisConfig {
 
         return RedisCacheManager.builder(redisConnectionFactory)
                 .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+
+    @Bean
+    public LettuceBasedProxyManager<String> proxyManager(RedisConnectionFactory connectionFactory) {
+        RedisClient redisClient = (RedisClient) ((LettuceConnectionFactory) connectionFactory).getNativeClient();
+        StatefulRedisConnection<String, byte[]> connection =
+                redisClient.connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE));
+        return LettuceBasedProxyManager.builderFor(connection)
                 .build();
     }
 
